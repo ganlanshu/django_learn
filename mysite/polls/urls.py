bd035ad2b61db3343cada9123534b6645a381aca
ganlanshu <grapebook0@163.com>
2016-03-15 11:10:21 +0800

    writing your first django app part 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
@@ -10,6 +11,6 @@ urlpatterns = [
     # ex: /polls/5/results/
     url(r'(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
-    url(r'(?P<question_id>[0-9]+/vote/$)', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 ]
